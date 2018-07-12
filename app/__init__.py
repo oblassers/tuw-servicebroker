87ddbadc0b0b80133853b405696c2ad95a84414a
Simon Oblasser <simon.oblasser@student.tuwien.ac.at>
2018-07-12 17:58:03 +0200

    fixed error with array of array of services when fetching a catalog. openbrokerapi requires exactly one Service per ServiceBroker class

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from openbrokerapi import api
 from openbrokerapi.log_util import basic_config
 
-from app.models import TuItServiceBroker
+from app.models import TuFilesServiceBroker, TuProCloudServiceBroker, TuHostServiceBroker
 
 
 def create_app():
     app = Flask(__name__)
     logger = basic_config()
 
-    openbroker_bp = api.get_blueprint(TuItServiceBroker(), api.BrokerCredentials("", ""), logger)
+    openbroker_bp = api.get_blueprint([TuFilesServiceBroker(), TuProCloudServiceBroker(), TuHostServiceBroker()],
+                                      api.BrokerCredentials("", ""), logger)
     app.register_blueprint(openbroker_bp)
 
     return app
